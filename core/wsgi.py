f67d92d2055b9812d9b438dfdbe6dc17a7d12fd3
Arthur <pyeye.91@gmail.com>
2016-11-15 17:50:17 +0500

    delete env variables settings

--- core/wsgi.py
+++ core/wsgi.py
@@ -7,18 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-import ast
 
 from django.core.wsgi import get_wsgi_application
-
-debug = os.getenv("DEBUG", "False")
-
-if ast.literal_eval(debug):
-    settings_module = 'core.settings.dev'
-else:
-    settings_module = 'core.settings.prod'
-
-os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
 
 application = get_wsgi_application()
