267e10322a4fadb885d9f9a926afdedc4b6c49de
Arthur <pyeye.91@gmail.com>
2016-11-22 19:55:09 +0500

    remove sending mail

--- apps/reservation/views.py
+++ apps/reservation/views.py
@@ -13,10 +13,9 @@ class ReservationAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save()
-        # send_mail(
-        #     'Новая бронь',
-        #     'Проверь админку',
-        #     settings.EMAIL_HOST_USER,
-        #     ['arturka-74@mail.ru', settings.EMAIL_HOST_USER],
-        #     fail_silently=False
-        # )
+        send_mail(
+            'Новая бронь',
+            'Проверь админку',
+            'reservation@maddogclub.com',
+            fail_silently=False
+        )
