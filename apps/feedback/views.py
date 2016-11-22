b752464db803095ac46f7f2f1534a221859c12d0
Arthur <pyeye.91@gmail.com>
2016-11-22 21:50:49 +0500

    send email about feedback

--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -1,7 +1,23 @@
 from rest_framework.generics import CreateAPIView
+from django.core.mail import send_mail
+from django.conf import settings
+
+from apps.base.auth import UnsafeSessionAuthentication
 
 from .serializers import FeedbackSerializer
 
 
 class FeedbackAPIView(CreateAPIView):
     serializer_class = FeedbackSerializer
+    authentication_classes = (UnsafeSessionAuthentication,)
+
+    def perform_create(self, serializer):
+        serializer.save()
+        send_mail(
+            'Новый отзыв',
+            'Проверь админку',
+            settings.EMAIL_HOST_USER,
+            ['feedback@maddogclub.com'],
+            fail_silently=False
+        )
+
