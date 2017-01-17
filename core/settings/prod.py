fd90e9f121aaa9db5912d251ee0f9ea57c12c3ca
Arthur <pyeye.91@gmail.com>
2017-01-17 22:19:45 +0500

    Fix cache

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY", False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
